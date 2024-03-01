215d7c1362ceac86f609cb211a8bde9c4d9574b5
Christian Johansen <christian@cjohansen.no>
2024-03-01 23:38:53 +0100

    Add form builder elements and scene

--- portfolio/virtuoso/elements/form_scenes.cljs
+++ portfolio/virtuoso/elements/form_scenes.cljs
@@ -2,25 +2,64 @@
   (:require [portfolio.replicant :refer [defscene]]
             [virtuoso.elements.form :as form]))
 
-(defscene form-box
-  (form/box
-   {}
-   (form/h2 "Exercise details")
-   (form/fields
-    (form/control
-     {:label "Length"}
-     (form/number-input {:value 4})
-     (form/select
-      {:values [{:value "beat" :text "Beats"}
-                {:value "bar" :selected? true :text "Bars"}
-                {:value "line" :text "Lines"}
-                {:value "phrase" :text "Phrases"}]}))
+(defscene form
+  (form/BoxedForm
+   {:boxes
+    [{:title "Exercise details"
+      :fields
+      [{:controls
+        [{:label "Length"
+          :inputs [{:input/kind :input.kind/number
+                    :value 4}
+                   {:input/kind :input.kind/select
+                    :values [{:value "beat" :text "Beats"}
+                             {:value "bar" :selected? true :text "Bars"}
+                             {:value "line" :text "Lines"}
+                             {:value "phrase" :text "Phrases"}]}]}
+         {:label "Time signature"
+          :inputs [{:input/kind :input.kind/number
+                    :value 4}
+                   {:input/kind :input.kind/select
+                    :values [{:value "4" :selected? true :text "4"}
+                             {:value "8" :text "8"}
+                             {:value "16" :text "16"}
+                             {:value "32" :text "32"}]}]}]}]}
 
-    (form/control
-     {:label "Time signature"}
-     (form/number-input {:value 4})
-     (form/select
-      {:values [{:value "4" :selected? true :text "4"}
-                {:value "8" :text "8"}
-                {:value "16" :text "16"}
-                {:value "32" :text "32"}]})))))
+     {:title "Session settings"
+      :fields
+      [{:controls
+        [{:label "Start at"
+          :inputs [{:input/kind :input.kind/select
+                    :values [{:value "forward" :selected? true :text "the top"}
+                             {:value "backward" :text "the end"}]}]}
+         {:label "Max phrase length"
+          :inputs [{:input/kind :input.kind/number
+                    :value 0}]}]}]}
+
+     {:title "Metronome settings"
+      :fields
+      [{:controls
+        [{:label "Start tempo"
+          :inputs [{:input/kind :input.kind/number
+                    :value 60}]}
+         {:label "BPM step"
+          :inputs [{:input/kind :input.kind/number
+                    :value 5}]}
+         {:label "Drop beats (%)"
+          :inputs [{:input/kind :input.kind/number
+                    :value 0}]}]}
+       {:controls
+        [{:label "Tick beats"
+          :inputs [{:input/kind :input.kind/pill-select
+                    :values [{:text "1" :selected? true}
+                             {:text "2" :selected? true}
+                             {:text "3" :selected? true}
+                             {:text "4" :selected? true}]}]}]}
+
+       {:controls
+        [{:label "Accentuate beats"
+          :inputs [{:input/kind :input.kind/pill-select
+                    :values [{:text "1" :selected? true}
+                             {:text "2"}
+                             {:text "3"}
+                             {:text "4"}]}]}]}]}]}))
--- src/virtuoso/elements/form.cljc
+++ src/virtuoso/elements/form.cljc
@@ -8,37 +8,62 @@
    (update attrs :class str " p-3 md:p-5 " layout/box-classes)
    content])
 
-(defn h2
-  ([text] (h2 nil text))
+(defn H2
+  ([text] (H2 nil text))
   ([_props text]
    [:h2.text-sm.mb-2.ml-1.font-bold
     text]))
 
-(defn fields [& fields]
+(defn Fields [& fields]
   [:div.flex.gap-4 fields])
 
-(defn control [{:keys [label]} & fields]
+(defn Control [{:keys [label]} & fields]
   [:label.form-control.w-40
    [:div.label
     [:span.label-text label]]
    [:div.flex.gap-2
     fields]])
 
-(defn number-input [{:keys [value]}]
+(defn NumberInput [{:keys [value]}]
   [:input.input.input-bordered.w-12.input-sm
    {:type "text" :value (str value)}])
 
-(defn select [{:keys [values]}]
+(defn Select [{:keys [values]}]
   [:select.select.select-bordered.w-26.select-sm
    (for [{:keys [value selected? text]} values]
      [:option (cond-> {:value value}
                 selected? (assoc :selected "selected"))
       text])])
 
-(defn pill-select [{:keys [values]}]
+(defn PillSelect [{:keys [values]}]
   [:div.flex.gap-2
    (for [{:keys [text selected?]} values]
      [:button.badge.badge-neutral
       {:class (when-not selected?
                 "badge-outline")}
       text])])
+
+(defn render-input [input]
+  (case (:input/kind input)
+    :input.kind/number (NumberInput input)
+    :input.kind/select (Select input)
+    :input.kind/pill-select (PillSelect input)))
+
+(defn BoxForm [form]
+  (box
+   {}
+   (some-> form :title H2)
+   (let [rows (for [{:keys [controls]} (:fields form)]
+                (let [fs (for [{:keys [label inputs]} controls]
+                           (Control {:label label}
+                                    (map render-input inputs)))]
+                  (if (= 1 (count fs))
+                    fs
+                    (Fields fs))))]
+     (if (= 1 (count rows))
+       rows
+       [:div.flex.flex-col.gap-4 rows]))))
+
+(defn BoxedForm [{:keys [boxes]}]
+  [:div.flex.flex-col.gap-4
+   (map BoxForm boxes)])
