0dbe2c4151035d9666829465a684a4f70dd3c1ce
Christian Johansen <christian@cjohansen.no>
2024-03-01 10:06:29 +0100

    Add a frontpage

--- src/virtuoso/pages/frontpage.clj
+++ src/virtuoso/pages/frontpage.clj
@@ -1,4 +1,18 @@
-(ns virtuoso.pages.frontpage)
+(ns virtuoso.pages.frontpage
+  (:require [virtuoso.ui.brain :as brain]
+            [virtuoso.ui.elements :as e]))
 
-(defn render-page [ctx page]
-  [:h1 "Hello"])
+(defn render-page [_ctx _page]
+  (e/layout
+   (e/flex-container
+    [:main.grow.flex.flex-col.justify-center
+     (brain/brain)
+     [:p.my-6.opacity-60
+      "Tools for musicians to practice more efficiently."]
+     (e/box {:href "/interleaved-clickup/"}
+      [:h2.mb-2 [:strong.font-bold "Interleaved Clicking Up"]]
+      [:p.opacity-80 "Solidify and bring a piece of music up to speed by leveraging random
+    practice while clicking up with the metronome."])]
+    [:footer
+     [:p.my-4.opacity-60
+      "Made by " [:a.link {:href "https://cjohansen.no"} "Christian Johansen"]]])))
