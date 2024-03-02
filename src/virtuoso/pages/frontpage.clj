75e02e3a3b4114476dfa5ee6e6a68c3cfd419e21
Christian Johansen <christian@cjohansen.no>
2024-03-02 08:33:48 +0100

    Use new namespaces on the frontpage

--- src/virtuoso/pages/frontpage.clj
+++ src/virtuoso/pages/frontpage.clj
@@ -1,18 +1,18 @@
 (ns virtuoso.pages.frontpage
- (:require [virtuoso.elements.brain :as brain]
-           [virtuoso.ui.elements :as e]))
+  (:require [virtuoso.elements.brain :as brain]
+            [virtuoso.elements.layout :as layout]))
 
 (defn render-page [_ctx _page]
-  (e/layout
-   (e/flex-container
+  (layout/layout
+   (layout/flex-container
     [:main.grow.flex.flex-col.justify-center
      (brain/brain)
-     [:p.my-6.opacity-60
+     [:p.my-6.opacity-80
       "Tools for musicians to practice more efficiently."]
-     (e/box {:href "/interleaved-clickup/"}
+     (layout/box {:href "/interleaved-clickup/"}
       [:h2.mb-2 [:strong.font-bold "Interleaved Clicking Up"]]
       [:p.opacity-80 "Solidify and bring a piece of music up to speed by leveraging random
     practice while clicking up with the metronome."])]
     [:footer
-     [:p.my-4.opacity-60
+     [:p.my-4.opacity-80
       "Made by " [:a.link {:href "https://cjohansen.no"} "Christian Johansen"]]])))
