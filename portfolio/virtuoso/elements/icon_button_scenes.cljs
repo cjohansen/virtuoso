2b3aaf6a0f40a9d8c1b5342e07cf3aa4cf1e0341
Christian Johansen <christian@cjohansen.no>
2024-07-31 09:40:35 +0200

    Rejig icon button sizes to make them more generally useful

--- portfolio/virtuoso/elements/icon_button_scenes.cljs
+++ portfolio/virtuoso/elements/icon_button_scenes.cljs
@@ -8,28 +8,71 @@
    {:text "Click"
     :icon (icons/icon :phosphor.regular/music-notes-plus)}))
 
-(defscene large-icon
-  (icon-button/icon-button
-   {:text "Click"
-    :icon (icons/icon :phosphor.regular/music-notes-plus)
-    :icon-size :large}))
+(defscene small-buttons
+  [:div.flex.gap-8
+   (icon-button/icon-button
+    {:text "Click"
+     :icon (icons/icon :phosphor.regular/music-notes-plus)
+     :icon-size :small
+     :size :small})
+   (icon-button/icon-button
+    {:text "Click"
+     :icon (icons/icon :phosphor.regular/music-notes-plus)
+     :icon-size :medium
+     :size :small})
+   (icon-button/icon-button
+    {:text "Click"
+     :icon (icons/icon :phosphor.regular/music-notes-plus)
+     :icon-size :large
+     :size :small})])
 
-(defscene large-button
-  (icon-button/icon-button
-   {:text "Click"
-    :icon (icons/icon :phosphor.regular/music-notes-plus)
-    :size :large}))
+(defscene medium-buttons
+  [:div.flex.gap-8
+   (icon-button/icon-button
+    {:text "Click"
+     :icon (icons/icon :phosphor.regular/play)
+     :icon-size :small
+     :size :medium})
+   (icon-button/icon-button
+    {:text "Click"
+     :icon (icons/icon :phosphor.regular/play)
+     :icon-size :medium
+     :size :medium})
+   (icon-button/icon-button
+    {:text "Click"
+     :icon (icons/icon :phosphor.regular/play)
+     :icon-size :large
+     :size :medium})])
 
-(defscene large-button-large-icon
-  (icon-button/icon-button
-   {:text "Click"
-    :icon (icons/icon :phosphor.regular/music-notes-plus)
-    :size :large
-    :icon-size :large}))
+(defscene large-buttons
+  [:div.flex.gap-8
+   (icon-button/icon-button
+    {:text "Click"
+     :icon (icons/icon :phosphor.regular/play)
+     :icon-size :small
+     :size :large})
+   (icon-button/icon-button
+    {:text "Click"
+     :icon (icons/icon :phosphor.regular/play)
+     :icon-size :medium
+     :size :large})
+   (icon-button/icon-button
+    {:text "Click"
+     :icon (icons/icon :phosphor.regular/play)
+     :icon-size :large
+     :size :large})])
 
-(defscene warn-theme
-  (icon-button/icon-button
-   {:text "Click"
-    :icon (icons/icon :phosphor.regular/music-notes-minus)
-    :icon-size :large
-    :theme :warn}))
+(defscene themes
+  [:div.flex.gap-8
+   (icon-button/icon-button
+    {:text "Click"
+     :icon (icons/icon :phosphor.regular/music-notes-simple)
+     :theme :info})
+   (icon-button/icon-button
+    {:text "Click"
+     :icon (icons/icon :phosphor.regular/music-notes-minus)
+     :theme :warn})
+   (icon-button/icon-button
+    {:text "Click"
+     :icon (icons/icon :phosphor.regular/music-notes-plus)
+     :theme :success})])
--- src/virtuoso/elements/icon_button.cljc
+++ src/virtuoso/elements/icon_button.cljc
@@ -1,25 +1,32 @@
 (ns virtuoso.elements.icon-button
   (:require [phosphor.icons :as icons]))
 
-(def size? #{:medium :large})
+(def size? #{:small :medium :large})
 
 (def button-icon-size
-  {:medium {:medium ["h-4" "w-4"]
+  {:small {:small ["h-3" "w-3"]
+           :medium ["h-4" "w-4"]
+           :large ["h-6" "w-6"]}
+   :medium {:small ["h-4" "w-4"]
+            :medium ["h-6" "w-6"]
             :large ["h-8" "w-8"]}
-   :large {:medium ["h-6" "w-6"]
+   :large {:small ["h-5" "w-5"]
+           :medium ["h-8" "w-8"]
            :large ["h-12" "w-12"]}})
 
 (def theme-class
-  {:normal "btn-info"
-   :warn "btn-error"})
+  {:info "btn-info"
+   :warn "btn-error"
+   :success "btn-success"})
 
 (defn icon-button [{:keys [text size theme icon icon-size actions disabled?]}]
   (let [size (if (size? size) size :medium)
         icon-size (if (size? icon-size) icon-size :medium)
-        theme (if (theme-class theme) theme :normal)]
+        theme (if (theme-class theme) theme :info)]
     [:div.btn.btn-circle
      {:title text
       :class [(when (= :large size) :btn-lg)
+              (when (= :small size) :btn-sm)
               (when disabled? :btn-disabled)
               (theme-class theme)]
       :on {:click actions}}
