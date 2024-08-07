93eb24301ac89dcaf0327cd421640e4b4a61a008
Christian Johansen <christian@cjohansen.no>
2024-08-07 16:14:38 +0200

    Fix the development window exception handler

--- dev/virtuoso/dev.cljs
+++ dev/virtuoso/dev.cljs
@@ -1,5 +1,6 @@
 (ns virtuoso.dev
-  (:require [gadget.inspector :as inspector]
+  (:require [clojure.string :as str]
+            [gadget.inspector :as inspector]
             [virtuoso.ui.main :as virtuoso]))
 
 (inspector/inspect "Application state" virtuoso/store)
@@ -10,8 +11,29 @@
 
 (set! virtuoso/*on-render* #(inspector/inspect (labels %1) %2))
 
-(defonce ^:export kicking-out-the-jams (virtuoso/boot))
+(defn print-error [e]
+  (js/console.error (.-message e))
+  (prn (ex-data e))
+  (println (.-stack e))
+  (when-let [cause (.-cause e)]
+    (println "Caused by:")
+    (print-error cause)))
 
-(set! js/window.-onerror
-      (fn [message source line-no col-no ex]
-        (prn "!!!!" message source line-no col-no ex)))
+(defonce ^:export kicking-out-the-jams
+  (do
+    (set! *print-namespace-maps* false)
+    (js/window.addEventListener
+     "error"
+     (fn [e]
+       (.preventDefault e)
+       (let [file (or (some-> (.-filename e)
+                              (str/split #"js/compiled")
+                              second)
+                      (.-filename e))]
+         (js/console.error "Uncaught exception in " (str file ":" (.-lineno e) ":" (.-colno e))))
+       (print-error (.-error e))))
+    (virtuoso/boot)))
+
+(comment
+  (set! *print-namespace-maps* false)
+)
